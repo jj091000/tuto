2c94caed9b3ca19bef78552dcb6b73832bb5fa20
Admin <Admin@192.168.2.21>
2017-09-16 19:01:55 -0400

    Espace corrigé

--- src/tp1/Tp1.java
+++ src/tp1/Tp1.java
@@ -7,7 +7,7 @@ import net.sf.json.JSONArray;
 public class Tp1 {
 
     public static void main(String[] args) {
-        System.out.print("Je m'appelle " + args[0] + args[1]);
+        System.out.print("Je m'appelle " + args[0] + " " + args[1]);
     }
     
 }
