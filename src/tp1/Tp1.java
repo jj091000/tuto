6f5632b96829f0fad48ab2c7d327d36f0c6401be
Admin <Admin@192.168.2.21>
2017-09-16 19:00:38 -0400

    Initial commit

---
+++ src/tp1/Tp1.java
@@ -0,0 +1,13 @@
+package tp1;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+
+public class Tp1 {
+
+    public static void main(String[] args) {
+        System.out.print("Je m'appelle " + args[0] + args[1]);
+    }
+    
+}
